0e9552b3e4b36bf453f1344bd5cc3a666f612d0a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-05-17 00:08:31 -0500

    Add variable arrays of each data type

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,15 @@
+# The 'push' command will add a new philosopher to the end of the variable array.
 heroes = ["Malcolm X", "Plato", "Aristotle", "Machiavelli"]
+heroes.push("Theodore Roosevelt")
+
+# The 'shift' command will remove an item from the beginning of the variable array.
 important_dates = [1776, 1931, 1916, 1964]
+important_dates.shift
+
+# The 'pop' command will remove an item from the end of the variable array.
 past_grades = [100.00, 92.50, 89.75, 23.50]
+past_grades.pop
+
+# The 'unshift' command will add an item to the beginning of the variable array.
 cats_in_boxes = [true, false, true, false]
+cats_in_boxes.unshift(false)
