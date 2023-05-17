43950665df1ab16b768656db67345fda9306e719
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-05-16 20:49:25 -0500

    Initial commit

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+heroes = ["Malcolm X", "Plato", "Aristotle", "Machiavelli"]
+important_dates = [1776, 1931, 1916, 1964]
+past_grades = [100.00, 92.50, 89.75, 23.50]
+cats_in_boxes = [true, false, true, false]
